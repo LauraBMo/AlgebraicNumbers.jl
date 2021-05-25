2756134d441f241710922d2e56a588d32a98896f
Laura Brustenga i Moncusí <brust@math.ku.dk>
2021-05-25 19:41:35 +0200

    more promote_rules

--- src/promote.jl
+++ src/promote.jl
@@ -5,8 +5,18 @@ import Base.promote_rule
 # Algebraic number from integer
 convert(::Type{T}, x::Number) where {T <: AlgebraicNumber} = AlgebraicNumber(x)
 
+
 # promotions
-promote_rule(::Type{T}, ::Type{AlgebraicNumber{S,F}}) where {T <: Number,S,F} = AlgebraicNumber{S,F}
+promote_rule(::Type{T}, ::Type{AlgebraicNumber{S,F}}) where {T <: Integer,S,F} =
+	AlgebraicNumber{promote_type(T, S),F}
+promote_rule(::Type{Rational{T}}, ::Type{AlgebraicNumber{S,F}}) where {T <: Integer,S,F} =
+	AlgebraicNumber{promote_type(T, S),F}
+promote_rule(::Type{Complex{T}}, ::Type{AlgebraicNumber{S,F}}) where {T <: Integer,S,F} =
+	AlgebraicNumber{promote_type(T, S),F}
+promote_rule(::Type{Complex{Rational{T}}, ::Type{AlgebraicNumber{S,F}}) where {T <: Integer,S,F} =
+	AlgebraicNumber{promote_type(T, S),F}
+premote_rule(::Type{AlgebraicNumber{T,F}},::Type{AlgebraicNumber{S,G}}) where {T,S <: Integer,F,G <: AbstractFloat} =
+	AlgebraicNumber{promote_type(T, S),promote_type(F, G)}
 
 isrational(an::AlgebraicNumber) = degree(an.coeff) == 1
 isinteger(an::AlgebraicNumber) = isrational(an) && abs(an.coeff[2]) == 1
