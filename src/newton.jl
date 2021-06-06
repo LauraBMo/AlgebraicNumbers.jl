00c5d8446e3ddb51e5bd736673d6a725a8f4b081
Laura Brustenga i Moncusí <brust@math.ku.dk>
2021-06-06 21:49:16 +0200

    fix poly_to_newton
    poly_to_newton depends on the degree of h, but it was implemented
    depending on the length of Newton(h). They usually agree but some times
    Newton(h) is shorter due to missing terms. In this case, we need to add
    roots at zero to the returned h.

--- src/newton.jl
+++ src/newton.jl
@@ -13,30 +13,27 @@ end
 # compute newton power series of polynomial given with coefficients coeff,
 # in base field R,x.
 # See fig.1 in reference
-function poly_to_newton(coeffs::Vector{T}, n, R, x) where {T <: Integer}
+function poly_to_newton(coeffs::Vector{T}, N, R, x) where {T <: Integer}
 	# first, make monic.
 	mcoeffs = moniccoeffs(coeffs)
-
-	d = degree(mcoeffs)
-	a_cfs = reverse(derivative(mcoeffs))
-	b_cfs = reverse(mcoeffs)
+	D = degree(mcoeffs)
 
 	# initialize power series polynomials
-	a = R(a_cfs)
-	b = R(b_cfs)
-	b0 = R(polyinv(b_cfs, n))
+	A = R(reverse(derivative(mcoeffs)))
+	B = R(reverse(mcoeffs))
 
-	c  = truncate(a * b0, d)
+	b0 = R(polyinv(reverse(mcoeffs), D))
+	c  = truncate(A * b0, D)
 
 	r = R()
 	x_power = R(1)
-	x_d = x^d
+	x_d = x^D
 
-	l = Int(floor(n / d))
-	for j = 0:l
+	step(C) = -mullow(shift_right(B * C, D), b0, D)
+	for j = 0:Int(floor(N / D))
 		r += c * x_power
 		x_power *= x_d
-		c = -mullow(shift_right(b * c, d), b0, d)
+		c = step(c)
 	end
 	return r
 end
@@ -46,18 +43,38 @@ end
 # tr: traces i.e. newton series
 # This algorithm is based on the Leverrier-Faddeev algorithm
 # see: http://math.stackexchange.com/questions/405822/what-is-the-fastest-way-to-find-the-characteristic-polynomial-of-a-matrix
-function newton_to_poly(tr::Vector{T}) where {T <: Number}
+#
+# function newton_to_poly(tr::Vector{T}) where {T <: Number}
+# 	# special case
+# 	out = T[0,1]
+# 	if tr != [1]
+# 		n = length(tr)
+# 		c = Vector{T}(undef, n)
+# 		c[end] = one(T)
+# 		for k = 1:n - 1
+# 			next_c = -sum(tr[2:(k + 1)] .* c[(end - k + 1):end]) // k
+# 			c[end - k] = next_c
+# 		end
+# 		out = c
+# 	end
+# 	return out
+# end
+
+function newton_to_poly(N::Vector{T}, D=length(N)) where {T <: Number}
 	# special case
+	n = length(N)
 	out = T[0,1]
-	if tr != [1]
-		n = length(tr)
+	if N != [1]
 		c = Vector{T}(undef, n)
 		c[end] = one(T)
 		for k = 1:n - 1
-			next_c = -sum(tr[2:(k + 1)] .* c[(end - k + 1):end]) // k
+			next_c = -sum(N[2:(k + 1)] .* c[(end - k + 1):end]) // k
 			c[end - k] = next_c
 		end
 		out = c
+	end
+	while length(out) < D
+		pushfirst!(out, zero(T))
 	end
 	return out
 end
@@ -76,7 +93,7 @@ function composed_product(p::Vector{T}, q::Vector{T}) where {T <: Integer}
 	b = poly_to_newton(q, n, R, x)
 
 	# multiply newton series and invert
-	pq = newton_to_poly(Hadamard(a, b))
+	pq = newton_to_poly(Hadamard(a, b), n)
 
 	# convert to integer and return
 	return convert_intcoeffs(T, pq)
@@ -87,41 +104,20 @@ composed_product(p,q) = composed_product(promote(p, q)...)
 # composed sum of two polynomials, given as coeffs p and q
 function composed_sum(p::Vector{T}, q::Vector{T}) where {T <: Integer}
 	# compute newton series
-	using Nemo
-    p = BigInt[80,0,0,70]
-	q = BigInt[-80,0,0,70]
-	n = AlgebraicNumbers.degree(p) * AlgebraicNumbers.degree(q) + 1
+	n = degree(p) * degree(q) + 1
 	R, x = Nemo.PolynomialRing(Nemo.FlintQQ, "x")
-	a = AlgebraicNumbers.poly_to_newton(p, n, R, x)
-	b = AlgebraicNumbers.poly_to_newton(q, n, R, x)
+	a = poly_to_newton(p, n, R, x)
+	b = poly_to_newton(q, n, R, x)
 
 	# exp series
 	ee  = R([1 // factorial(BigInt(i)) for i = 0:n])
 	eei = R([factorial(BigInt(i)) for i = 0:n])
 
 	# multiply newton series and invert
-	m = mullow(R(AlgebraicNumbers.Hadamard(a, ee)), R(AlgebraicNumbers.Hadamard(b, ee)), n + 1)
-	pq = AlgebraicNumbers.newton_to_poly(AlgebraicNumbers.Hadamard(m, eei))
-	AlgebraicNumbers.convert_intcoeffs(pq)
+	m = mullow(R(Hadamard(a, ee)), R(Hadamard(b, ee)), n + 1)
+	pq = newton_to_poly(Hadamard(m, eei), n)
 	# convert to integer and return
 	return convert_intcoeffs(T, pq)
 end
 
-AlgebraicNumbers.composed_sum([80,0,0,70], [-80,0,0,70])
 composed_sum(p,q) = composed_sum(promote(p, q)...)
-
-	n = (length(p) - 1) * (length(q) - 1) + 1
-	R, x = Nemo.PolynomialRing(Nemo.FlintQQ, "x")
-	a = to_newton(p, n, R, x)
-	b = to_newton(q, n, R, x)
-
-	# exp series
-	ee  = R([Nemo.FlintQQ(1 // factorial(BigInt(i))) for i = 0:n])
-	eei = R([Nemo.FlintQQ(factorial(BigInt(i))) for i = 0:n])
-
-	# multiply newton series and invert
-	m = mullow(hadm(a, ee, R), hadm(b, ee, R), n + 1)
-	pq = from_newton(to_array(hadm(m, eei, R)))
-
-	# convert to integer and return
-	return map(numerator, pq * lcm(map(denominator, pq)))
